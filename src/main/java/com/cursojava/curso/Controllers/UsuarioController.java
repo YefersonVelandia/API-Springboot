b5f4e5cb76eaf90441fd2e5db8aa3abdd2f4e8c1
yeferson <yefersonvelandia4@gmail.com>
2022-01-29 10:20:03 -0500

    added user list

--- src/main/java/com/cursojava/curso/Controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/Controllers/UsuarioController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController //Anotación
 public class UsuarioController {
 
@@ -22,6 +25,39 @@ public class UsuarioController {
         objUsuario.setTelefono(34434343);
 
         return objUsuario;
+    }
+
+    @RequestMapping(value = "usuarios") //Ruta que devolvera el usuario
+    public List<Usuario> getusuarios(@PathVariable Long id){
+           List<Usuario> usuarios = new ArrayList<>();
+
+        Usuario objUsuario = new Usuario();
+        objUsuario.setId(id);
+        objUsuario.setNombre("pedro");
+        objUsuario.setApellido("fernandez");
+        objUsuario.setEmail("correo@correo.com");
+        objUsuario.setTelefono(34434343);
+
+        Usuario objUsuario2 = new Usuario();
+        objUsuario2.setId(id);
+        objUsuario2.setNombre("ana");
+        objUsuario2.setApellido("vallon");
+        objUsuario2.setEmail("correo@correo.com");
+        objUsuario2.setTelefono(34434343);
+
+
+        Usuario objUsuario3 = new Usuario();
+        objUsuario3.setId(id);
+        objUsuario3.setNombre("jejeje");
+        objUsuario3.setApellido("jijiji");
+        objUsuario3.setEmail("correo@correo.com");
+        objUsuario3.setTelefono(34434343);
+
+        usuarios.add(objUsuario);
+        usuarios.add(objUsuario2);
+        usuarios.add(objUsuario3);
+
+        return usuarios;
     }
 
     @RequestMapping(value = "usuarioeditar") //Ruta que devolvera el usuario
