15e52bb1803a406676e193b4da7c90262dd02f52
yeferson <yefersonvelandia4@gmail.com>
2022-01-29 12:21:23 -0500

    call to server endpoint

--- src/main/java/com/cursojava/curso/Controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/Controllers/UsuarioController.java
@@ -28,18 +28,18 @@ public class UsuarioController {
     }
 
     @RequestMapping(value = "usuarios") //Ruta que devolvera el usuario
-    public List<Usuario> getusuarios(@PathVariable Long id){
+    public List<Usuario> getusuarios(){
            List<Usuario> usuarios = new ArrayList<>();
 
         Usuario objUsuario = new Usuario();
-        objUsuario.setId(id);
+        objUsuario.setId(1L);
         objUsuario.setNombre("pedro");
         objUsuario.setApellido("fernandez");
         objUsuario.setEmail("correo@correo.com");
         objUsuario.setTelefono(34434343);
 
         Usuario objUsuario2 = new Usuario();
-        objUsuario2.setId(id);
+        objUsuario2.setId(2L);
         objUsuario2.setNombre("ana");
         objUsuario2.setApellido("vallon");
         objUsuario2.setEmail("correo@correo.com");
@@ -47,7 +47,7 @@ public class UsuarioController {
 
 
         Usuario objUsuario3 = new Usuario();
-        objUsuario3.setId(id);
+        objUsuario3.setId(3L);
         objUsuario3.setNombre("jejeje");
         objUsuario3.setApellido("jijiji");
         objUsuario3.setEmail("correo@correo.com");
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
 async function cargarUsuarios(){
 
-        const request = await fetch('usuario/3', {
+        const request = await fetch('usuarios', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -20,5 +20,16 @@ async function cargarUsuarios(){
 
         console.log(usuarios);
 
+        let listadoHtml = '';
+       for (let usuario  of usuarios){
+           let formatoUsuario = ' <tr><td>'+usuario.id+'</td><td> '+usuario.nombre+' '+usuario.apellido+'<td>'+usuario.email+'</td><td>'+usuario.telefono+'</td><td>\n' +
+               '                                                <a href="#" class="btn btn-danger btn-circle btn-sm">\n' +
+               '                                                    <i class="fas fa-trash"></i>\n' +
+               '                                                </a>\n' +
+               '                                            </td></tr> ' ;
+
+           listadoHtml += formatoUsuario;
+       }
+    document.querySelector('#usuarios tbody').innerHTML = listadoHtml;
 }
 
